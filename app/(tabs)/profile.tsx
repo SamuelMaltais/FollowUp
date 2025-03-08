194fb7398fb54a4442b9efd3526c03ef6820f7c1
Samuel Maltais <74563135+SamuelMaltais@users.noreply.github.com>
2025-03-08 13:12:14 -0500

    Initial tab structure

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,9 +1,9 @@
 import { Text, View, StyleSheet } from "react-native";
 
-export default function Calendar() {
+export default function Profile() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>About screen</Text>
+      <Text style={styles.text}>Profile</Text>
     </View>
   );
 }
