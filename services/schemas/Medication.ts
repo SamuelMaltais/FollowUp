0ba99629b45e3685ea59ff0ad233e11d5be783ca
alessmcs <alessandramancas23@gmail.com>
2025-03-08 21:25:06 -0500

    calc all dates for a med

--- services/schemas/Medication.ts
+++ services/schemas/Medication.ts
@@ -21,6 +21,7 @@ export class Medication {
     amount: number;
     dosage: number;
     lastTakenDate: Date;
+    allDosages : Date[] = [];
     constructor(patientName: string, medicationName: string, consumptionDetails: string, prescriptionDate: string, expDate: string, interval: string, amount: number, dosage: number, lastTakenDate: string) {
         this.uuid = uuidv4();
         this.patientName = patientName;
@@ -68,5 +69,30 @@ export class Medication {
             obj.dosage,
             obj.lastTakenDate
         );
+    }
+
+    calculateNextDosageDate(interval: string, lastDate: Date): Date {
+        const nextDosageDate = new Date(lastDate);
+        if (interval.includes("hours")) {
+            const intervalHours = parseInt(interval, 10); // Assuming interval is in hours
+            nextDosageDate.setHours(nextDosageDate.getHours() + intervalHours);
+        } else {
+            const intervalDays = parseInt(interval, 10); // Assuming interval is in days
+            nextDosageDate.setDate(nextDosageDate.getDate() + intervalDays);
+        }
+        return nextDosageDate;
+    }
+
+    calcDosages() {
+        let dates: Date[] = []; // array to store all the dates
+        let currDate = new Date(this.prescriptionDate); // calc all dosage instances between now and when the prescription ends
+
+        while (currDate < this.expDate) {
+            console.log(currDate);
+            currDate = this.calculateNextDosageDate(this.interval, currDate); // Assign the new date directly
+            dates.push(new Date(currDate)); // Push a new Date object to avoid mutating currDate
+        }
+        console.log(dates);
+        this.allDosages = dates;
     }
 }
